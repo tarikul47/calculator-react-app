2e86b7419688569c1b777cd4ad1951f14e4f242e
Tarikul Islam <tarikul47@gmail.com>
2021-12-18 17:23:04 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return (
+  const [result, setResult] = useState('');
+  const handleClick = e => {
+    setResult(result.concat(e.target.name));
+  }
+  const handleClear = () => {
+    setResult('')
+  }
+  const handleCalculate = () => {
+    setResult(eval(result).toString());
+  }
+    return (
     <div class="container my-4">
       <hr class="mb-5" />
 
@@ -9,65 +20,65 @@ function App() {
         <input
           type="text"
           class="calculator-screen z-depth-1"
-          value=""
+          value={result}
           disabled
         />
 
         <div class="calculator-keys">
-          <button type="button" class="operator btn btn-info" value="+">
+          <button name="+" onClick={handleClick} type="button" class="operator btn btn-info" value="+">
             +
           </button>
-          <button type="button" class="operator btn btn-info" value="-">
+          <button name="-" onClick={handleClick} type="button" class="operator btn btn-info" value="-">
             -
           </button>
-          <button type="button" class="operator btn btn-info" value="*">
+          <button name="*" onClick={handleClick} type="button" class="operator btn btn-info" value="*">
             &times;
           </button>
-          <button type="button" class="operator btn btn-info" value="/">
+          <button name="/" onClick={handleClick} type="button" class="operator btn btn-info" value="/">
             &divide;
           </button>
 
-          <button type="button" value="7" class="btn btn-light waves-effect">
+          <button name="7" onClick={handleClick} type="button" value="7" class="btn btn-light waves-effect">
             7
           </button>
-          <button type="button" value="8" class="btn btn-light waves-effect">
+          <button name="8" onClick={handleClick} type="button" value="8" class="btn btn-light waves-effect">
             8
           </button>
-          <button type="button" value="9" class="btn btn-light waves-effect">
+          <button name="9" onClick={handleClick} type="button" value="9" class="btn btn-light waves-effect">
             9
           </button>
 
-          <button type="button" value="4" class="btn btn-light waves-effect">
+          <button name="4" onClick={handleClick} type="button" value="4" class="btn btn-light waves-effect">
             4
           </button>
-          <button type="button" value="5" class="btn btn-light waves-effect">
+          <button name="5" onClick={handleClick} type="button" value="5" class="btn btn-light waves-effect">
             5
           </button>
-          <button type="button" value="6" class="btn btn-light waves-effect">
+          <button name="6" onClick={handleClick} type="button" value="6" class="btn btn-light waves-effect">
             6
           </button>
 
-          <button type="button" value="1" class="btn btn-light waves-effect">
+          <button name="1" onClick={handleClick} type="button" value="1" class="btn btn-light waves-effect">
             1
           </button>
-          <button type="button" value="2" class="btn btn-light waves-effect">
+          <button name="2" onClick={handleClick} type="button" value="2" class="btn btn-light waves-effect">
             2
           </button>
-          <button type="button" value="3" class="btn btn-light waves-effect">
+          <button name="3" onClick={handleClick} type="button" value="3" class="btn btn-light waves-effect">
             3
           </button>
 
-          <button type="button" value="0" class="btn btn-light waves-effect">
+          <button name="0" onClick={handleClick} type="button" value="0" class="btn btn-light waves-effect">
             0
           </button>
-          <button
+          <button name="." onClick={handleClick}
             type="button"
             class="decimal function btn btn-secondary"
             value="."
           >
             .
           </button>
-          <button
+          <button name="" onClick={handleClear}
             type="button"
             class="all-clear function btn btn-danger btn-sm"
             value="all-clear"
@@ -75,7 +86,7 @@ function App() {
             AC
           </button>
 
-          <button
+          <button name="" onClick={handleCalculate}
             type="button"
             class="equal-sign operator btn btn-default"
             value="="
